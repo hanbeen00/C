79bdcb66966797f455f30211ee22b96ff780ebe1
hanbeen00 <76528601+hanbeen00@users.noreply.github.com>
2023-07-16 21:56:31 +0900

    Create 2-2.c

---
+++ C프로그래밍및실습/실습/2장(변수와 자료형)/2-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+	double m = 4.200000;
+	double d = 5.100000;
+	printf("first number: %lf\n", m);
+	printf("second number: %lf\n", d);
+	printf("sum: %lf", m + d);
+}
