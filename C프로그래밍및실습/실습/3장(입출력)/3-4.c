605b65cbb46b2524c16c5be14f173ca89b7d29da
hanbeen00 <76528601+hanbeen00@users.noreply.github.com>
2023-07-16 22:16:56 +0900

    Create 3-4.c

---
+++ C프로그래밍및실습/실습/3장(입출력)/3-4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("purchase 1:%6d won\n", a);
+	printf("purchase 2:%6d won\n", b);
+	printf("purchase 3:%6d won\n", c);
+}
