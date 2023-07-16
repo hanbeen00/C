ecd022a1aa09c5a25f7e501a90c2acd4ce415662
hanbeen00 <76528601+hanbeen00@users.noreply.github.com>
2023-07-16 22:12:16 +0900

    Create 2-5.c

---
+++ C프로그래밍및실습/실습/2장(변수와 자료형)/2-5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+	printf("The double quotation mark is \".\n");
+	printf("The single quotation mark is \'.\n");
+	printf("Character A is \'A\'.\n");
+	printf("To represent a double quotation mark, use the escape sequence \\\".");
+}
